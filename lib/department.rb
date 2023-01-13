59018528e213c554a2041260712716d341b6cb24
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-13 09:20:52 -0700

    Add hired employees

--- lib/department.rb
+++ lib/department.rb
@@ -1,7 +1,13 @@
 class Department
-  attr_reader :name
+  attr_reader :name, 
+              :employees
 
   def initialize(name)
     @name = name
+    @employees = []
+  end
+
+  def hire(employee)
+    @employees << employee
   end
 end
--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,7 +3,10 @@ require './lib/employee'
 
 RSpec.describe Department do
   let(:customer_service) { Department.new("Customer Service") }
-  describe '#initialize' do
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
+
+  describe '#Initialize' do
     it 'exists' do
       
       expect(customer_service).to be_a(Department)
@@ -11,6 +14,19 @@ RSpec.describe Department do
 
     it "has attributes" do
       expect(customer_service.name).to eq("Customer Service")
+    end
+  end
+
+  describe "#Employees" do
+    it "starts with no employees" do
+      expect(customer_service.employees).to eq([])
+    end
+
+    it "can hire employees" do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
