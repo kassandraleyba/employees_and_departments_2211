6442ca1bc29d6b6c858c4f2ae725e0c8f4680ee5
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-13 09:57:52 -0700

    Add budget current year

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,16 @@
 class Department
   attr_reader :name, 
               :employees,
-              :expenses
+              :expenses,
+              :current_year,
+              :budget
 
   def initialize(name)
     @name = name
     @employees = []
     @expenses = 0
+    @current_year = Time.now.year
+    @budget = 0
   end
 
   def hire(employee)
